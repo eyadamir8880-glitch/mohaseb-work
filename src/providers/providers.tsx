b3a13f7f1f080c5811f4a0cf434d4b92c3765dd4
eyadamir8880-glitch <eyadamir8880@gmail.com>
2026-06-22 00:16:13 +0300

    Fix providers.tsx dead import

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,8 +1,7 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from './theme-provider';
 import { LanguageProvider } from './language-provider';
-import { AppInit } from './app-init';
 import { ToastContainer } from '@/components/layout/toast-container';
 import { ToastProvider } from '@/hooks/use-toast-provider';
 
@@ -11,7 +10,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <ThemeProvider>
       <LanguageProvider>
         <ToastProvider>
-          <AppInit />
           <ToastContainer />
           {children}
         </ToastProvider>
